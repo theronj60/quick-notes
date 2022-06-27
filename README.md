81128ff3f2bd91ca2888777bb6227499084489f9
Theron Joe <theronj@joetwebdev.io>
2022-06-27 00:16:40 -0600

    added main function and created checkDir function to check if a directory exists. Need to add create dir for when it doesnt exist.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-#add quick notes from terminal
+## add quick notes from terminal
 -   create a note that is saved into a default file
 -	different file can be used for saving
 -	-p flag, persist, keep program open and keep saving single line notes to file
 -	create TODO md files, quickly. Add title, then enter line, hit enter. move to next save to a README for example
 -	check note for title, if note doesnt have one, prompt for one.
+-	create file in current dir or specify location (default current dir)
--- main.go
+++ main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
-	// "encoding/json"
-	// "fmt"
-	// "io/ioutil"
-	// "log"
-	// "net/http"
-	// "os"
-
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	// "github.com/manifoldco/promptui"
 )
 
-func main() {
+// look for directory /{home}/.notes/
+// save dir to variable
+// save file in variable dir
 
+func checkDir(home string) bool {
+	_, err := os.Stat(home + "/quick-notes")
+
+	if os.IsNotExist(err) {
+		fmt.Println("Could not find directory.")
+		os.Exit(3)
+	}
+	fmt.Println("Found Directory.")
+	return true
+}
+
+func main() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if(checkDir(home)) {
+		files, err := ioutil.ReadDir(home + "/quick-notes")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, file := range files {
+			fmt.Println(file.Name(), file.IsDir())
+		}
+	}
 }
